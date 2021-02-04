dc0930592cec692e1b079355f175c35d93ac867f
Cameron Wall <cwall@redhat.com>
2021-02-04 09:37:33 -0500

    Docker changes and readme addition

---
+++ entrypoint-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" = 'scan' ]; then
+    ./login.sh $2 $3 $4
+    ./memory-scanner-sharpened.sh $5
+elif [ "$1" = 'diff' ]; then
+    if [ -z "${5}"]; then
+        python3 ./spot-the-difference-sharpened.py -f $2 -s $3 -o $4
+    else
+        python3 ./spot-the-difference-sharpened.py -f $2 -s $3 -o $4 -c $5
+    fi
+else
+    echo "please use either scan or diff"
+fi
